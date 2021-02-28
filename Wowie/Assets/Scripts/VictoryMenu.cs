0c6bc787fe7390aa52f3edd8b58a9d132f22c4bb
Nrigz <franciscocosta21@tecnico.ulisboa.pt>
2021-02-28 13:14:51 +0000

    Added Victory Menu

---
+++ Wowie/Assets/Scripts/VictoryMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VictoryMenu : MonoBehaviour
+{
+    [SerializeField] private AudioSource clickSound;
+    public void GoToMainMenu()
+    {
+        clickSound.Play();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void PlayAgain()
+    {
+        clickSound.Play();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Fábio");
+    }
+}
